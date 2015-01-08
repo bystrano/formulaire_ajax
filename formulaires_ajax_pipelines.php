7495b4d323211ab3f7214c2c3b50dbc95dc38be3
Michel Bystranowski <bystrano@gmx.ch>
2015-01-08 19:37:59 +0100

    commit initial - généré par la Fabrique

---
+++ formulaires_ajax_pipelines.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Utilisations de pipelines par jQuery Formulaires SPIP
+ *
+ * @plugin     jQuery Formulaires SPIP
+ * @copyright  2015
+ * @author     Michel
+ * @licence    GNU/GPL
+ * @package    SPIP\Formulaires_ajax\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
---
+++ lang/paquet-formulaires_ajax_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // F
+    'formulaires_ajax_description' => '',
+    'formulaires_ajax_nom' => 'jQuery Formulaires SPIP',
+    'formulaires_ajax_slogan' => 'une boîte à outils javascript pour les formulaires ajax générés avec Saisies',
+);
