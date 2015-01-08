66b4fa88ea49819efcaefa1938232aec3ec782f1
Michel Bystranowski <bystrano@gmx.ch>
2015-01-08 20:36:56 +0100

    Structure générale du plugin jquery

--- javascripts/jquery.formulairesAjax.js
+++ javascripts/jquery.formulairesAjax.js
@@ -1,11 +1,25 @@
 (function ($) {
     $.fn.formulairesAjax = function (options) {
 
-        var config = $.extend({
-            // These are the defaults.
-            // color: "#556b2f",
-            // backgroundColor: "white"
+        var config, form;
+
+        config = $.extend({
+            // Options par défaut
+            blocsAjax: [] // Un tableau de noms ajax de <INCLURE>
         }, options);
+
+        // retourne un objet contenant les valeurs des saisies du
+        // formulaire
+        this.valeurs = function () {
+            return 'TODO retourner les valeurs du formulaire';
+        }
+
+        // s'il n'y a pas de blocs ajax on ne fait rien
+        if (config.blocsAjax.length === 0) {
+            return this;
+        }
+
+        // TODO ajaxifier le formulaire
 
         return this;
     };
