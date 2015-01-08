be587c3f202417e5ff0e2a78c21fe94b1f31bfc3
Michel Bystranowski <bystrano@gmx.ch>
2015-01-08 21:34:42 +0100

    fonction pour retourner les valeurs du formulaire
    Pour l'instant ça ne marche que quand on a de la chance et pour les
    saisies checkbox, mais on fournit une API pour définir une fonction pour
    chaque type de saisie via options.calculerValeurSaisie

--- javascripts/jquery.formulairesAjax.js
+++ javascripts/jquery.formulairesAjax.js
@@ -1,26 +1,68 @@
 (function ($) {
     $.fn.formulairesAjax = function (options) {
 
-        var config, form;
+        var config, form, champs;
+
+        form = $(this);
 
         config = $.extend({
             // Options par défaut
-            blocsAjax: [] // Un tableau de noms ajax de <INCLURE>
+            blocsAjax: [], // Un tableau de noms ajax de <INCLURE>
+            // Donne des fonctions pour trouver la valeur d'une saisie
+            calculValeurSaisie: {
+                defaut: function (champ) {
+                    return $(champ).find('input').attr('value');
+                },
+                checkbox: function (champ) {
+                    var inputs = $(champ).find('input'),
+                        valeur = [];
+
+                    inputs.each(function () {
+                        if ($(this).attr('checked') === 'checked') {
+                            valeur.push($(this).attr('value'));
+                        }
+                    });
+
+                    return valeur;
+                }
+            }
         }, options);
+
+        // Retourne une fonction qui calcule la valeur d'un type de
+        // saisie donné
+        form.calcul_valeur = function (type_saisie) {
+
+            if (config.calculValeurSaisie.hasOwnProperty(type_saisie)) {
+                return config.calculValeurSaisie[type_saisie];
+            }
+
+            return config.calculValeurSaisie.defaut;
+        }
 
         // retourne un objet contenant les valeurs des saisies du
         // formulaire
-        this.valeurs = function () {
-            return 'TODO retourner les valeurs du formulaire';
+        form.valeurs = function () {
+            var valeurs = {};
+
+            champs = form.find('.editer');
+
+            champs.each(function () {
+                var type_saisie = this.className.replace(/^.*saisie_([^ ]+).*$/,'$1'),
+                    nom_saisie = this.className.replace(/^.*editer_([^ ]+).*$/,'$1');
+
+                valeurs[nom_saisie] = (form.calcul_valeur(type_saisie))(this);
+            });
+
+            return valeurs;
         }
 
         // s'il n'y a pas de blocs ajax on ne fait rien
         if (config.blocsAjax.length === 0) {
-            return this;
+            return form;
         }
 
         // TODO ajaxifier le formulaire
 
-        return this;
+        return form;
     };
 }(jQuery));
